036473643e7ad7d2add91c8c477d045aab4634c7
dsmith265 <72399454+dsmith265@users.noreply.github.com>
2020-10-05 08:00:30 -0700

    Add files via upload

---
+++ AddingMachine.java
@@ -0,0 +1,33 @@
+package cse360assignment02;
+
+public class AddingMachine {
+	
+	//Total summation variable
+	private int total;
+	
+	//construct
+	public AddingMachine() {
+		total = 0;
+	}
+	
+	//get summation function
+	public int getTotal() {			
+		return total;
+	}
+	
+	//summation function 
+	public void add(int value) {	
+	}
+    
+	//return value as a string
+	public String toString() {	
+		return "";
+	}
+	
+	//resets the machine
+	public void clear() {	
+	}
+		
+}
+	
+
